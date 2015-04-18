8f09d1240dfad62838a9c8a54f3bb05144ce2242
Nicolas DUBIEN <github@dubien.org>
2015-04-19 01:12:34 +0200

    Add Room and RoomManager in order to handle multiplayer

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+#!/usr/bin/node
 /**
  *	Node.JS server for Online-PacMan game
  */
 
 var app = require('express')(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	path = require('path');
 
-var pc = require("./www/pacman");
+var _room = require('./www/room.js'),
+	Room = _room.Room,
+	RoomManager = require('./www/room_manager.js').RoomManager;
+
+var __smartfilename = path.basename(__filename);
+
+// Add ability to access line number
+// http://stackoverflow.com/questions/11386492/accessing-line-number-in-v8-javascript-chrome-node-js
+Object.defineProperty(global, '__stack', {
+	get: function(){
+		var orig = Error.prepareStackTrace;
+		Error.prepareStackTrace = function(_, stack){ return stack; };
+		var err = new Error;
+		Error.captureStackTrace(err, arguments.callee);
+		var stack = err.stack;
+		Error.prepareStackTrace = orig;
+		return stack;
+	}
+});
+Object.defineProperty(global, '__line', {
+	get: function(){
+		return __stack[1].getLineNumber();
+	}
+});
+
+var _log = function(line, description) {
+	return __smartfilename + '\t:' + line + '\t' + description;
+};
 
 // Serve static files: homepage, js, css, favicon...
 app
@@ -23,46 +52,90 @@ app
 	res.sendfile(__dirname + '/static/js/pacman.js');
 });
 
-var game = undefined;
+var room_manager = new RoomManager(io);
+
 io.sockets.on('connection', function(socket) {
-	// New game
-	socket.on('new', function() {
-		console.log('New game session');
-		game = new pc.Game(io, socket.id);
-		game.refresh();
+	room_manager.register(socket.id);
+	
+	// Unregister the socket from the underlying RoomManager
+	socket.on('close', function () {
+		room_manager.disconnect(socket.id);
+	});
+
+	// Rename the user
+	socket.on('change_username', function(username) {
+		room_manager.changeUsername(socket.id, username);
+	});
+
+	// Join an existing room
+	socket.on('join_room', function(room_id) {
+		room_manager.joinRoom(socket.id, room_id);
+	});
+
+	// Create a new room
+	socket.on('create_room', function() {
+		room_manager.createRoom(socket.id);
+	});
+
+	// Leave a room
+	socket.on('leave_room', function() {
+		room_manager.leaveRoom(socket.id);
+	});
+
+	// Ask to launch the game inside the room
+	// The game will not start immediately and other members can cancel its launch
+	socket.on('run_game', function() {
+		var room = room_manager.getRoom(socket.id);
+		if (room) {
+			room.runGame();
+		}
 	});
 	
+	// Cancel game
+	socket.on('cancel_game', function() {
+		var room = room_manager.getRoom(socket.id);
+		if (room) {
+			room.cancelGame();
+		}
+	});
+
+	// Run the game @depreciated
+	socket.on('new', function() {
+		room_manager.createRoom(socket.id);
+		var room = room_manager.getRoom(socket.id);
+		room.runGame();
+	});
+
 	// Start the game
 	socket.on('start', function() {
-		if (! game) {
-			console.error('Unable to start the session: game has not been initialized');
+		var room = room_manager.getRoom(socket.id);
+		if (! room) {
+			console.log(_log(__line, 'Room is not defined for #' + socket.id));
 			return;
 		}
-
-		console.log('Start the session');
-		game.start();
+		room.startGame(socket.id);
 	});
 	
 	// Update the direction of the player
 	socket.on('keydown', function(keycode) {
-		if (! game) {
-			console.warn('Unable to listen to keydown: game has not been initialized');
+		var room = room_manager.getRoom(socket.id);
+		if (! room) {
 			return;
 		}
 
 		console.log('Update direction based on "' + keycode + '"');
 		if (keycode == 37) {
 			console.log('+-> Left');
-			game.setPacmanDirection(pc.LEFT);
+			room.receiveKeyboard(socket.id, _room.LEFT);
 		} else if (keycode == 38) {
 			console.log('+-> Up');
-			game.setPacmanDirection(pc.UP);
+			room.receiveKeyboard(socket.id, _room.UP);
 		} else if (keycode == 39) {
 			console.log('+-> Right');
-			game.setPacmanDirection(pc.RIGHT);
+			room.receiveKeyboard(socket.id, _room.RIGHT);
 		} else if (keycode == 40) {
 			console.log('+-> Down');
-			game.setPacmanDirection(pc.DOWN);
+			room.receiveKeyboard(socket.id, _room.DOWN);
 		}
 	});
 });
---
+++ www/room.js
@@ -0,0 +1,145 @@
+(function() {
+
+var assert = require('assert');
+
+var pc = require('./pacman.js'),
+	Game = pc.Game;
+
+var Room = function(io, manager) {
+	assert(io);
+	assert(manager);
+
+	Room.STATUS_JOIN = 0;
+	Room.STATUS_WAIT = 1;
+	Room.STATUS_PLAY = 2;
+
+	Room.WAIT_TIME_MS = 10000;
+
+	Room.MAX_NUM_PLAYERS = 4;
+	
+	var io = io;
+	var self = this;
+	var manager_ = manager;
+	
+	var status_ = Room.STATUS_JOIN;
+	var members_ = new Array();
+	var game_ = undefined;
+	var timeout_run_ = undefined;
+
+	// Serialize current object for JSON export
+	// @return dictionary representing the room
+	this.toDictionary = function() {
+		return {
+				'status': status_ == Room.STATUS_JOIN
+					? 'join'
+					: (status_ == Room.STATUS_WAIT
+						? 'wait'
+						: 'play'),
+				'users': members_,
+				'is_full': self.isFull(),
+		};
+	};
+
+	// Does the Room contain members?
+	// @return true if the room does not have any members
+	this.isEmpty = function() {
+		return members_.length == 0;
+	};
+
+	// Does the room is fully populated?
+	// @return true if the room is full
+	this.isFull = function() {
+		return members_.length == Room.MAX_NUM_PLAYERS;
+	};
+	
+	// Instantiate a Game
+	// Launch the game session
+	var realRunGame = function() {
+		assert.equal(status_, Room.STATUS_WAIT);
+		assert(members_.length);
+		
+		status_ = Room.STATUS_PLAY;
+		game_ = new Game(io, members_[0]);
+		game_.refresh();
+
+		manager.notifyChanges();
+	};
+
+	// Run a new game session
+	// The game is not started immediately and can still be canceled by other players of the room
+	// during Room.WAIT_TIME_MS ms
+	// @warning makes a call to manager.notifyChanges
+	// 			please keep in mind to keep a stable state for manager when calling this method
+	this.runGame = function() {
+		assert.equal(status_, Room.STATUS_JOIN);
+		assert(members_.length);
+		
+		status_ = Room.STATUS_WAIT;
+		timeout_run_ = setTimeout(realRunGame, Room.WAIT_TIME_MS);
+
+		manager.notifyChanges();
+	};
+	
+	// Cancel the Game (before it started)
+	// @warning makes a call to manager.notifyChanges
+	// 			please keep in mind to keep a stable state for manager when calling this method
+	this.cancelGame = function() {
+		assert.equal(status_, Room.STATUS_WAIT);
+		assert(timeout_run_);
+		
+		clearTimeout(timeout_run_);
+		status_ = Room.STATUS_JOIN;
+
+		manager.notifyChanges();
+	};
+	
+	// Transfer the start message directly towards the Game
+	this.startGame = function(sid) {
+		assert.equal(status_, Room.STATUS_PLAY);
+		assert.notEqual(members_.indexOf(sid), -1);
+		assert(game_);
+
+		game_.start();
+	};
+
+	// Add a player to the Room (if and only if the room is not already full)
+	// Throw an assert if the player if already in the room
+	// @return true if the player was successfully added
+	this.join = function(sid) {
+		assert.equal(status_, Room.STATUS_JOIN);
+		assert.equal(members_.indexOf(sid), -1);
+
+		if (self.isFull()) {
+			return false;
+		}
+
+		members_.push(sid);
+		return true;
+	};
+
+	// Remove a player from the room
+	this.leave = function(sid) {
+		assert.equal(status_, Room.STATUS_JOIN); //TODO remove and kill the game
+		var id = members_.indexOf(sid);
+		assert.notEqual(id, -1);
+
+		members_.splice(id, 1);
+	};
+
+	// Send arrows to the game
+	this.receiveKeyboard = function(sid, arrow) {
+		assert.notEqual(members_.indexOf(sid), -1);
+
+		if (status_ == Room.STATUS_PLAY) {
+			game_.setPacmanDirection(arrow);
+		}
+	};
+};
+
+module.exports.Room = Room;
+module.exports.LEFT = pc.LEFT;
+module.exports.RIGHT = pc.RIGHT;
+module.exports.UP = pc.UP;
+module.exports.DOWN = pc.DOWN;
+}());
+
