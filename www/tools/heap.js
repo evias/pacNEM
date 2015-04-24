f33ffa1dd570eb73fcd6eb772cfa0d5c737eb4db
Nicolas DUBIEN <github@dubien.org>
2015-04-24 23:38:50 +0200

    Private methods and parameters for Heap

--- www/tools/heap.js
+++ www/tools/heap.js
@@ -6,76 +6,76 @@
  */
 
 var Heap = function() {
-	this.elements = new Array();
-	this.num_elements = 0;
+	var elements_ = new Array();
+	var num_elements_ = 0;
 	
 	this.push = function(heap_element) {
 		// room available?
-		if (this.elements.length > this.num_elements)
-			this.elements[this.num_elements] = heap_element;
+		if (elements_.length > num_elements_)
+			elements_[num_elements_] = heap_element;
 		else
-			this.elements.push(heap_element);
+			elements_.push(heap_element);
 		
-		this.num_elements++;
-		this.moveUp(this.num_elements -1);
+		num_elements_++;
+		moveUp(num_elements_ -1);
 	};
 	
 	this.pop = function() {
-		if (this.num_elements == 0) {
+		if (num_elements_ == 0) {
 			return undefined;
 		}
 
-		var head_heap_elt = this.elements[0];
-		this.num_elements--;
+		var head_heap_elt = elements_[0];
+		num_elements_--;
 
-		if (this.num_elements == 0)
+		if (num_elements_ == 0)
 			return head_heap_elt;
 		
-		this.elements[0] = this.elements[this.num_elements];
-		this.elements.splice(this.num_elements, 1);
-		this.moveDown(0);
+		elements_[0] = elements_[num_elements_];
+		elements_.splice(num_elements_, 1);
+		moveDown(0);
 		return head_heap_elt;
 	};
 	
 	this.free = function() {
-		this.elements = new Array();
-		this.num_elements = 0;
+		elements_ = new Array();
+		num_elements_ = 0;
 	};
 	
 	this.size = function() {
-		return this.num_elements;
+		return num_elements_;
 	};
 	
-	this.moveUp = function(id) {
+	var moveUp = function(id) {
 		if (id == 0)
 			return;
 
 		var parent_id = Math.floor((id -1)/2);
-		if (this.elements[id].dist < this.elements[parent_id].dist) {
-			var tmp_heap_elt = this.elements[id];
-			this.elements[id] = this.elements[parent_id];
-			this.elements[parent_id] = tmp_heap_elt;
-			this.moveUp(parent_id);
+		if (elements_[id].dist < elements_[parent_id].dist) {
+			var tmp_heap_elt = elements_[id];
+			elements_[id] = elements_[parent_id];
+			elements_[parent_id] = tmp_heap_elt;
+			moveUp(parent_id);
 		}
 	};
 	
-	this.moveDown = function(id) {
+	var moveDown = function(id) {
 		var child1_id = id*2 +1;
-		if (child1_id >= this.num_elements) // it does not have any child
+		if (child1_id >= num_elements_) // it does not have any child
 			return;
 		
 		var child_id = child1_id;
 		var child2_id = id*2 +2;
-		if (child2_id < this.num_elements) { // only one child
-			if (this.elements[child2_id].dist < this.elements[child1_id].dist) {
+		if (child2_id < num_elements_) { // only one child
+			if (elements_[child2_id].dist < elements_[child1_id].dist) {
 				child_id = child2_id;
 			}
 		}
-		if (this.elements[child_id].dist < this.elements[id].dist) {
-			var tmp_heap_elt = this.elements[id];
-			this.elements[id] = this.elements[child_id];
-			this.elements[child_id] = tmp_heap_elt;
-			this.moveDown(child_id);
+		if (elements_[child_id].dist < elements_[id].dist) {
+			var tmp_heap_elt = elements_[id];
+			elements_[id] = elements_[child_id];
+			elements_[child_id] = tmp_heap_elt;
+			moveDown(child_id);
 		}
 	};
 };
